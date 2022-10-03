836b704cbaaee5419434bc51c472cbb72edab023
Parth Kakadiya <parthkakadiya320@gmail.com>
2022-10-03 17:25:46 +0200

    Price Counter

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -12,8 +12,17 @@ const controls = [
 
 const buildControls = (props) => (
   <div className={classes.BuildControls}>
+    <p>
+      Current Price: <strong>{props.price.toFixed(2)}</strong>
+    </p>
     {controls.map((ctrl) => (
-      <BuildControl key={ctrl.lable} lable={ctrl.lable} />
+      <BuildControl
+        key={ctrl.lable}
+        lable={ctrl.lable}
+        added={() => props.ingredientAdded(ctrl.type)}
+        removed={() => props.ingredientRemoved(ctrl.type)}
+        disabled={props.disabled[ctrl.type]}
+      />
     ))}
   </div>
 );
